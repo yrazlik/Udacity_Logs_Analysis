bc545ddb65158bc16ac65fbec295707ed5a9a011
Yasin Razlik <yrazlik@gmail.com>
2018-10-01 00:30:44 +0300

    Code Review

--- newsdata.py
+++ newsdata.py
@@ -2,14 +2,14 @@ import psycopg2
 
 # What are the most popular three articles of all time?
 query_top_articles = """SELECT articles.title, COUNT(*) as viewCount
-    FROM articles JOIN log 
+    FROM articles JOIN log
     ON log.path LIKE concat('%', articles.slug, '%')
     WHERE log.status LIKE '%200%'
     GROUP BY articles.title
     ORDER BY viewCount DESC
     LIMIT 3;"""
 
-# Who are the most popular article authors of all time? 
+# Who are the most popular article authors of all time?
 query_top_authors = """SELECT authors.name, COUNT(*) as viewCount
     FROM authors JOIN articles
     ON authors.id = articles.author
@@ -20,7 +20,7 @@ query_top_authors = """SELECT authors.name, COUNT(*) as viewCount
     LIMIT 3;
     """
 
-# On which days did more than 1% of requests lead to errors? 
+# On which days did more than 1% of requests lead to errors?
 query_errors = """SELECT total.day,
     ROUND(((errors.error_count * 1.0) / (total.requests * 1.0)), 3) AS percent
     FROM (SELECT date_trunc('day', time) "day", COUNT(*) AS error_count
@@ -33,9 +33,11 @@ query_errors = """SELECT total.day,
         GROUP BY day)
     AS total
     ON total.day = errors.day
-    WHERE (ROUND(((errors.error_count * 1.0) / (total.requests * 1.0)), 3) > 0.01)
+    WHERE
+    (ROUND(((errors.error_count * 1.0) / (total.requests * 1.0)), 3) > 0.01)
     ORDER BY percent DESC;
     """
+
 
 # Query the db
 def execute_query(query):
@@ -46,25 +48,30 @@ def execute_query(query):
     connection.close()
     return result_rows
 
+
 # Get the top three articles
 def get_top_three_articles():
     top_three_articles = execute_query(query_top_articles)
     print ("\n" + "*** TOP 3 ARTICLES ***")
 
-    order = 1;
+    order = 1
     for title, viewCount in top_three_articles:
-        print(str(order) + ". " + "\"" + title + "\"" + " -- " + str(viewCount) + " views.")
+        print(str(order) + ". " + "\"" + title + "\"" +
+              " -- " + str(viewCount) + " views.")
         order += 1
+
 
 # Get the top authors
 def get_top_authors():
     top_three_authors = execute_query(query_top_authors)
     print ("\n" + "*** TOP AUTHORS ***")
 
-    order = 1;
+    order = 1
     for name, viewCount in top_three_authors:
-        print(str(order) + ". " + "\"" + name + "\"" + " -- " + str(viewCount) + " views.")
+        print(str(order) + ". " + "\"" + name + "\"" +
+              " -- " + str(viewCount) + " views.")
         order += 1
+
 
 # Get the daays did more than 1% of requests lead to errors
 def get_error_days():
